bc64b28802c73ce18fec451b831f15a7ce997484
Martin Andersen <martin_haugaard8@hotmail.com>
2019-05-07 18:17:19 +0200

    Much of the game logic is working. Can now find the winner

---
+++ PokerTestProgram/HandLogic.cs
@@ -0,0 +1,205 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokerTestProgram
+{
+    // https://github.com/SaarSch/Texas_Holdem/blob/master/TexasHoldem/TexasHoldem/Logics/HandLogic.cs
+    public class HandLogic
+    {
+        public HandStrength HandCalculator(List<Card> cards)
+        {
+            int handValue;
+            HandRank handRank;
+            var hand = new List<Card>();
+            var orderedByValue = cards.OrderBy(card => card.Value).ToList();
+            var boost = (int) Math.Pow(10, 6);
+
+            //Look for similar cards:
+            var threesList = new List<Card>();
+            var pairsList = new List<Card>();
+            var foursList = new List<Card>();
+            var i = 0;
+            while (i < 6)
+            {
+                if (orderedByValue.ElementAt(i).Value == orderedByValue.ElementAt(i + 1).Value)
+                {
+                    if (i == 5 || orderedByValue.ElementAt(i + 2).Value != orderedByValue.ElementAt(i).Value)
+                    {
+                        pairsList.AddRange(orderedByValue.GetRange(i, 2));
+                        i = i + 2;
+                        continue;
+                    }
+                    if (i < 4 && orderedByValue.ElementAt(i + 3).Value == orderedByValue.ElementAt(i).Value)
+                    {
+                        foursList.AddRange(orderedByValue.GetRange(i, 4));
+                        break;
+                    }
+                    threesList.AddRange(orderedByValue.GetRange(i, 3));
+                    i = i + 3;
+                    continue;
+                }
+                i++;
+            }
+            if (pairsList.Count == 6) pairsList.RemoveRange(0, 2);    // if there's more than 2 pairs, remove the pair of least value
+            if (threesList.Count == 6) threesList.RemoveRange(0, 3);  // if there's 2x Three of a kind, remove the three of a kind of least value
+
+            //Look for similar shape:
+            var sameShapeList = cards.Where(card => card.Suit == Suit.Clubs).OrderBy(card => card.Value).ToList();
+            if (sameShapeList.Count < 5)
+                sameShapeList = cards.Where(card => card.Suit == Suit.Diamonds)
+                    .OrderBy(card => card.Value)
+                    .ToList();
+            if (sameShapeList.Count < 5)
+                sameShapeList = cards.Where(card => card.Suit == Suit.Hearts).OrderBy(card => card.Value).ToList();
+            if (sameShapeList.Count < 5)
+                sameShapeList = cards.Where(card => card.Suit == Suit.Spades).OrderBy(card => card.Value).ToList();
+
+            //Look for ascending
+            var ascending = new List<Card>();
+            for (var j = 0; j < 6; j++)
+            for (var q = j + 1; q < 7; q++)
+            {
+                var tempOrdered = new List<Card>();
+                tempOrdered.AddRange(orderedByValue);
+                tempOrdered.RemoveAt(q);
+                tempOrdered.RemoveAt(j);
+
+                var tempAscending = 0;
+                for (var m = 0; m < 4; m++)
+                    if (tempOrdered[m].Value + 1 == tempOrdered[m + 1].Value) tempAscending++;
+                if (tempAscending == 4 && SumListCard(ascending) < SumListCard(tempOrdered))
+                    ascending = tempOrdered;
+            }
+
+            //Decide Hand
+            var temp = IsStraightFlush(ascending, sameShapeList);
+            if (temp != null)
+            {
+                hand = temp;
+                handRank = hand.ElementAt(0).Value == 10 ? HandRank.RoyalFlush : HandRank.StraightFlush;
+                handValue = CalculateHandValue(hand, 8 * boost);
+            }
+            else if (foursList.Count == 4)
+            {
+                handRank = HandRank.FourOfAKind;
+                orderedByValue.RemoveAll(card => foursList.Contains(card));
+                hand.Add(orderedByValue.ElementAt(2));
+                hand.AddRange(foursList);
+                handValue = CalculateHandValue(hand, 7 * boost);
+            }
+            else if (threesList.Count == 3 && pairsList.Count >= 2)
+            {
+                handRank = HandRank.FullHouse;
+                hand.AddRange(pairsList.GetRange(pairsList.Count - 2, 2));
+                hand.AddRange(threesList);
+                handValue = CalculateHandValue(hand, 6 * boost);
+            }
+            else if (sameShapeList.Count >= 5)
+            {
+                hand.AddRange(sameShapeList.GetRange(sameShapeList.Count - 5, 5));
+                handRank = HandRank.Flush;
+                handValue = CalculateHandValue(hand, 5 * boost);
+            }
+            else if (ascending.Count >= 5)
+            {
+                hand.AddRange(ascending.GetRange(ascending.Count - 5, 5));
+                handRank = HandRank.Straight;
+                handValue = CalculateHandValue(hand, 4 * boost);
+            }
+            else if (threesList.Count == 3)
+            {
+                handRank = HandRank.ThreeOfAKind;
+                orderedByValue.RemoveAll(card => threesList.Contains(card));
+                hand.AddRange(orderedByValue.GetRange(2, 2));
+                hand.AddRange(threesList);
+                handValue = CalculateHandValue(hand, 3 * boost);
+            }
+            else
+            {
+                switch (pairsList.Count)
+                {
+                    case 4:
+                        handRank = HandRank.TwoPair;
+                        orderedByValue.RemoveAll(card => pairsList.Contains(card));
+                        hand.Add(orderedByValue.ElementAt(2));
+                        hand.AddRange(pairsList);
+                        handValue = CalculateHandValue(hand, 2 * boost);
+                        break;
+                    case 2:
+                        handRank = HandRank.Pair;
+                        orderedByValue.RemoveAll(card => pairsList.Contains(card));
+                        hand.AddRange(orderedByValue.GetRange(2, 3));
+                        hand.AddRange(pairsList);
+                        handValue = CalculateHandValue(hand, boost);
+                        break;
+                    default:
+                        handRank = HandRank.HighCard;
+                        hand.AddRange(orderedByValue.GetRange(2, 5));
+                        handValue = CalculateHandValue(hand, 0);
+                        break;
+                }
+            }
+            return new HandStrength(handValue, handRank, hand);
+        }
+
+        public int CalculateHandValue(List<Card> hand, int boost)
+        {
+            var ans = boost;
+            for (var i = 0; i < 5; i++)
+                ans = ans + (int) Math.Pow(10, i) * hand.ElementAt(i).Value;
+            return ans;
+        }
+
+        private List<Card> IsStraightFlush(List<Card> ascending, List<Card> similarShape)
+        {
+            if (ascending.Count < 5 || similarShape.Count < 5) return null;
+            return ascending.Count == 5 ? IsStrightFlushHelper(ascending, similarShape) : null;
+        }
+
+        private List<Card> IsStrightFlushHelper(List<Card> ascending, List<Card> similarShape)
+        {
+            var hand = new List<Card>();
+            hand.AddRange(ascending);
+            ascending.RemoveAll(similarShape.Contains);
+            return ascending.Count == 0 ? hand : null;
+        }
+
+        private int SumListCard(List<Card> cards)
+        {
+            var sum = 0;
+            for (var i = 0; i < cards.Count; i++) sum += cards[i].Value;
+            return sum;
+        }
+    }
+    
+    // https://github.com/SaarSch/Texas_Holdem/blob/master/TexasHoldem/TexasHoldem/Game/HandStrength.cs
+    public enum HandRank
+    {
+        RoyalFlush,
+        StraightFlush,
+        FourOfAKind,
+        FullHouse,
+        Flush,
+        Straight,
+        ThreeOfAKind,
+        TwoPair,
+        Pair,
+        HighCard,
+        Fold
+    }
+
+    public class HandStrength
+    {
+        public List<Card> HandCards;
+        public HandRank Handrank;
+        public int HandStrongestValue;
+
+        public HandStrength(int handValue, HandRank handRank, List<Card> cards)
+        {
+            HandStrongestValue = handValue;
+            HandCards = cards;
+            Handrank = handRank;
+        }
+    }
+}
---
+++ PokerTestProgram/Program.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokerTestProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var deck = new CardDeck();
+            var pokerRoom = new PokerRoom();
+            var dealer = new Dealer(pokerRoom, deck);
+
+            pokerRoom.AddPlayerToRoom(new Player("Phil Ivey", 100));
+            pokerRoom.AddPlayerToRoom(new Player("Phil Hellmuth", 100));
+            pokerRoom.AddPlayerToRoom(new Player("Phil Laak", 100));
+            
+            dealer.DistributeBlinds();
+            dealer.TakePaymentFromBlinds();
+            dealer.DealCards();
+            
+            dealer.DrawFlopCards();
+            dealer.DrawTurnCard();
+            dealer.DrawRiverCard();
+
+            dealer.FindWinner();
+        }
+    }
+
+    // https://exceptionnotfound.net/modeling-the-card-game-war-in-c-part-2-the-code/
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+    public enum Blinds
+    {
+        SmallBlind,
+        BigBlind,
+        NoBlind
+    }
+    
+    public class Card
+    {
+        public Suit Suit { get; private set; }
+        
+        public int Value { get; private set; }
+
+        public Card(Suit suit, int value)
+        {
+            Suit = suit;    
+            Value = value;
+        }
+    }
+
+    public class Player
+    {
+        public string Alias { get; private set; }
+        private List<Card> PlayerHand = new List<Card>();
+        public int ChipsAmount { get; set; }
+        public Blinds Blind { get; set; } = Blinds.NoBlind;
+
+        public Player(string alias, int chipsAmount)
+        {
+            Alias = alias;
+            ChipsAmount = chipsAmount;
+        }
+
+        public void GiveCardsToPlayer(List<Card> cards)
+        {
+            PlayerHand.AddRange(cards);
+        }
+
+        public List<Card> SeePlayerCards()
+        {
+            return PlayerHand;
+        }
+
+        public void RemoveCardsFromPlayer()
+        {
+            PlayerHand = new List<Card>();
+        }
+    }
+
+    public class CardDeck
+    {
+        private List<Card> cards = new List<Card>();
+
+        public CardDeck()
+        {
+            ResetDeck();
+        }
+
+        public void ResetDeck()
+        {
+            cards = new List<Card>();
+            
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                for (var value = 1; value <= 13; value++)
+                {
+                    cards.Add(new Card(suit, value));
+                }
+            }
+        }
+
+        public Card PeekDeck()
+        {
+            return cards.First();
+        }
+
+        public Card TakeTopCard()
+        {
+            var cardToGive = cards.First();
+            cards.Remove(cardToGive);
+            return cardToGive;
+        }
+
+        // https://stackoverflow.com/a/1262619
+        public void ShuffleCards(int numberOfShuffles = 1)
+        {
+            var rnd = new Random();
+
+            for (int i = 0; i < numberOfShuffles; i++)
+            {
+                var n = cards.Count;
+                while (n > 1)
+                {
+                    n--;
+                    int k = rnd.Next(n + 1);
+                    var value = cards[k];
+                    cards[k] = cards[n];
+                    cards[n] = value;
+                }
+            }
+        }
+    }
+
+    public class PokerRoom
+    {
+        public List<Player> players = new List<Player>();
+        public List<Card> communityCards = new List<Card>();
+        public int Pot { get; set; }
+
+        public void AddPlayerToRoom(Player newPlayer)
+        {
+            players.Add(newPlayer);
+        }
+
+        public void AddCommunityCard(Card communityCard)
+        {
+            communityCards.Add(communityCard);
+        }
+
+        public void AddToPot(int chipsAmount)
+        {
+            Pot += chipsAmount;
+        }
+
+        public void ResetRoom()
+        {
+            players = new List<Player>();
+            communityCards = new List<Card>();
+            Pot = 0;
+        }
+    }
+
+    public class Dealer
+    {
+        private PokerRoom _pokerRoom;
+        private CardDeck _cardDeck;
+        private HandLogic _handLogic = new HandLogic();
+        
+        public Dealer(PokerRoom pokerRoom, CardDeck cardDeck)
+        {
+            _pokerRoom = pokerRoom;
+            _cardDeck = cardDeck;
+        }
+
+        public void DistributeBlinds()
+        {
+            if (IsFirstRound())
+            {
+                _pokerRoom.players[0].Blind = Blinds.SmallBlind;
+                _pokerRoom.players[1].Blind = Blinds.BigBlind;
+            }
+            else
+            {
+                for (int i = 0; i < _pokerRoom.players.Count; i++)
+                {
+                    if (_pokerRoom.players[i].Blind == Blinds.BigBlind)
+                    {
+                        // Pass on the big blind to the next player
+                        if (i + 1 < _pokerRoom.players.Count)
+                        {
+                            _pokerRoom.players[i + 1].Blind = Blinds.BigBlind;
+                        }
+                        else
+                        {
+                            _pokerRoom.players.First().Blind = Blinds.BigBlind;
+                        }
+                        
+                        // Remove the small blind from the previous player
+                        if(i - 1 >= 0)
+                        {
+                            _pokerRoom.players[i - 1].Blind = Blinds.NoBlind;
+                        }
+                        else
+                        {
+                            _pokerRoom.players.Last().Blind = Blinds.NoBlind;
+                        }
+                        
+                        //Assign small blind to the previous big blind
+                        _pokerRoom.players[i].Blind = Blinds.SmallBlind;
+                        
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void TakePaymentFromBlinds()
+        {
+            foreach (var pokerPlayer in _pokerRoom.players)
+            {
+                if (pokerPlayer.Blind == Blinds.SmallBlind)
+                {
+                    pokerPlayer.ChipsAmount -= 10;
+                    _pokerRoom.AddToPot(10);
+                }
+                else if (pokerPlayer.Blind == Blinds.BigBlind)
+                {
+                    pokerPlayer.ChipsAmount -= 20;
+                    _pokerRoom.AddToPot(20);
+                }
+            }
+        }
+
+        public void DealCards()
+        {
+            _cardDeck.ShuffleCards(10);
+
+            _cardDeck.TakeTopCard(); // Remove the first card
+            
+            foreach (var pokerPlayer in _pokerRoom.players)
+            {
+                var cardsToGive = new List<Card>();
+                
+                cardsToGive.Add(_cardDeck.TakeTopCard());
+                cardsToGive.Add(_cardDeck.TakeTopCard());
+                
+                pokerPlayer.GiveCardsToPlayer(cardsToGive);
+            }
+        }
+
+        public void DrawFlopCards()
+        {
+            DrawCommunityCard(true);
+            DrawCommunityCard();
+            DrawCommunityCard();
+        }
+
+        public void DrawTurnCard()
+        {
+            DrawCommunityCard(true);
+        }
+
+        public void DrawRiverCard()
+        {
+            DrawCommunityCard(true);
+        }
+
+        public Player FindWinner()
+        {
+            //var communityRank = _handLogic.HandCalculator(_pokerRoom.communityCards);
+            
+            var handRankings = new List<HandStrength>();
+            
+            foreach (var pokerPlayer in _pokerRoom.players)
+            {
+                var playerHandAndCommunityCards = new List<Card>();
+                playerHandAndCommunityCards.AddRange(pokerPlayer.SeePlayerCards());
+                playerHandAndCommunityCards.AddRange(_pokerRoom.communityCards);
+                
+                handRankings.Add(_handLogic.HandCalculator(playerHandAndCommunityCards));
+            }
+            
+            return _pokerRoom.players[0];
+        }
+        
+        private void DrawCommunityCard(bool removeFirstCard = false)
+        {
+            if(removeFirstCard) 
+                _cardDeck.TakeTopCard();
+            
+            _pokerRoom.AddCommunityCard(_cardDeck.TakeTopCard());
+        }
+
+        private bool IsFirstRound()
+        {
+            foreach (var pokerPlayer in _pokerRoom.players)
+            {
+                if (pokerPlayer.Blind != Blinds.NoBlind)
+                {
+                    return false; // It's not the first round if some players already have blinds
+                }
+            }
+
+            return true;
+        }
+    }
+}
